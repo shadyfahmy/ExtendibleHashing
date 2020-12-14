f0440e4dfa72fcb0cbc1aaf9211e5ef298474ad0
3amrA7med <amr.ahmed.abdelbaqi@gmail.com>
2020-12-14 03:18:33 +0200

    Added Display and search functionalties to extendible hashing algorithm, Added test function to main

--- ExtendibleHashing.cpp
+++ ExtendibleHashing.cpp
@@ -4,7 +4,7 @@
  * Input: key used to calculate the hash
  * Output: HashValue;
  */
-int hashCodeChaining(int key){
+int hashCode(int key){
    return key % NUMBER_OF_BUCKETS;
 }
 
@@ -12,7 +12,47 @@ int insertItem(int dirFd, int bucketsFd,Record item){
 }
 
 int searchItem(int dirFd, int bucketsFd, Record item, int *count) {
+   
+   Directory data;
+   Bucket bucketData;
+   ssize_t result = pread(dirFd,&data,sizeof(Directory), 0);
 
+   // Case global depth is equal 0, Read first bucket 
+   if(data.globalDepth == 0) {
+      result = pread(dirFd,&bucketData,sizeof(Bucket), 0);
+      for(int i = 0; i< RECORDS_PER_BUCKET; i++){
+         count++;
+         if ((bucketData.records[1].valid == 1) && (bucketData.records[1].key == item.key))
+            return 0;                                                    // Offset of the bucket
+      }
+      // Item not found 
+      return -1;
+   }
+
+   // Case any depth
+   int hashedKey = hashCode(item.key);
+
+   bitset<MAX_BITS_IN_DIRECTORY> x(hashedKey);
+   string keyBits = x.to_string().substr(MAX_BITS_IN_DIRECTORY-data.globalDepth,MAX_BITS_IN_DIRECTORY-1);
+   
+
+   result = pread(bucketsFd,&bucketData,sizeof(Bucket), data.elements[stoi(keyBits)].bucketOffset);
+   if(result < 0)
+   { 	  
+      perror("some error occurred in pread");
+      return -1;
+   }
+
+   for(int i = 0; i< RECORDS_PER_BUCKET; i++){
+      count++;
+      if ((bucketData.records[i].valid == 1) && (bucketData.records[i].key == item.key))
+         return data.elements[stoi(keyBits)].bucketOffset; 
+   }
+   // Not found.
+   return -1;
+
+   
+   
 }
 int deleteItem(int dirFd, int bucketsFd, Record item , int *count){
 
@@ -20,16 +60,32 @@ int deleteItem(int dirFd, int bucketsFd, Record item , int *count){
 int display(int dirFd, int bucketsFd){
    // Displaye Directory
    Directory data;
+   Bucket bucketData;
+   int count = 0;
    ssize_t result = pread(dirFd,&data,sizeof(Directory), 0);
    if(result < 0)
    { 	  perror("some error occurred in pread");
          return -1;
    } else
    {
+      cout << "-------Directory-------" << endl;
+      cout << "Global Depth = " << data.globalDepth << endl;
+      cout << endl;
       for(int i =0; i< (int)pow(2,data.globalDepth); i++)
       {
          bitset<MAX_BITS_IN_DIRECTORY> x(data.elements[i].key);
-         cout << "Element#" << i+1 << "-Key: " << x.to_string()[0,data.globalDepth] << ", Offset: " << data.elements[i].bucketOffset << endl;
+         cout << "Element#" << i+1 << "  Key= " << x.to_string().substr(MAX_BITS_IN_DIRECTORY-data.globalDepth,MAX_BITS_IN_DIRECTORY-1)  << ", Offset= " << data.elements[i].bucketOffset << endl;
+         ssize_t result = pread(bucketsFd,&bucketData,sizeof(Bucket), data.elements[i].bucketOffset);
+         cout << "Bucket Data: Local Depth=" << bucketData.localDepth << endl;
+         for(int j=0; j< RECORDS_PER_BUCKET; j++){
+            count ++;
+            if (bucketData.records[j].valid == 1)
+               cout << "Record#" << i+1 << "  Key= " << bucketData.records[j].key << ", Value=" << bucketData.records[j].value << endl;
+            else 
+               cout << "Record#" << i+1 << " => empty" << endl;
+         }
+         cout << endl;
       }
+      return count;
    }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "readfile.h"
 #include<iostream>
 
+#include <iostream>
+#include <math.h>
+void Test(int, int);
+
 using namespace std;
 
 int main(void)
@@ -8,6 +12,10 @@ int main(void)
 
     int directoryFileHandler = createFile(DIRECTORY_FILESIZE,"Directory");
     int bucketsFileHandler = createFile(BUCKETS_FILESIZE,"Buckets");
+    int result;
+
+    Test(directoryFileHandler,  bucketsFileHandler);
+    display(directoryFileHandler, bucketsFileHandler);
 
     while(1){
         int operation;
@@ -18,15 +26,17 @@ int main(void)
         cout << "1 -> Search" << endl;
         cout << "2 -> Insert" << endl;
         cout << "3 -> Delete" << endl;
+        cout << "4 -> Display" << endl;
         cin >> operation;
         cout << "Enter a key" << endl;
-        cin >> key;
-        if(operation == 2)
-        {
-            cout<<"Enter a value" << endl;
-            cin >> val;
+        if(operation != 4) {
+            cin >> key;
+            if(operation == 2)
+            {
+                cout<<"Enter a value" << endl;
+                cin >> val;
+            }
         }
-        
         Record r;
         r.key = key;
         r.valid = 1;
@@ -35,7 +45,11 @@ int main(void)
         switch (operation)
         {
         case 1:
-            searchItem(directoryFileHandler , bucketsFileHandler, r, &count);
+            result = searchItem(directoryFileHandler , bucketsFileHandler, r, &count);
+            if(result != -1)
+                cout << "Record Found at offset = " << result << ", Number of records accessed is " << count << endl;
+            else
+                cout << "Item Not found, number of records accessed is " << count << endl;
             break;
                 
         case 2:
@@ -45,14 +59,47 @@ int main(void)
         case 3:
             deleteItem(directoryFileHandler , bucketsFileHandler, r, &count);
             break;    
+        case 4:
+            // Display directory and buckets
+            result = display(directoryFileHandler, bucketsFileHandler);
+            cout << "Number of records accessed is " << result << endl;
+            break; 
 
         default:
             break;
         }
-        // Display directory and buckets
-        display(directoryFileHandler, bucketsFileHandler);
+        
     }
 
 
     return 0;
+}
+
+
+void Test(int directoryFileHandler, int bucketsFileHandler){ 
+    Directory dir;
+    dir.globalDepth = 2;
+    dir.elements[0].key = 0;
+    dir.elements[0].bucketOffset = 0;
+
+    dir.elements[1].key = 1;
+    dir.elements[1].bucketOffset = 40;
+    
+    dir.elements[2].key = 2;
+    dir.elements[2].bucketOffset = 80;
+
+    dir.elements[3].key = 3;
+    dir.elements[3].bucketOffset = 120;
+    int result = pwrite(directoryFileHandler,&dir,sizeof(Directory), 0);
+
+    Bucket b;
+    b.localDepth = 2;
+    b.records[0].valid = 1;
+    b.records[0].key=32;
+    b.records[0].value=15;
+    
+    b.records[1].valid = 1;
+    b.records[1].key=64;
+    b.records[1].value=444;
+    result = pwrite(bucketsFileHandler,&b ,sizeof(Bucket), 0);
 }
